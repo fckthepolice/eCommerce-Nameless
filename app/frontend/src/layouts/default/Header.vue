4246129f285ab3087c29575f84b295392f0bdd1a
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-08-02 16:09:50 -0300

    fix header

--- app/frontend/src/layouts/default/Header.vue
+++ app/frontend/src/layouts/default/Header.vue
@@ -5,9 +5,35 @@
                 <img class="brand" src="/icons/nameless-icon-white.svg">
             </v-btn>
         </v-toolbar-title>
-        <v-btn icon v-for="item in menu" :key="item.path" :to="item.path">
-            <img class="icon-img" :src="`${item.src}`">
-        </v-btn>
+        <div class="icons-xl w-25 d-flex justify-space-around">
+            <v-btn icon to="/map">
+                <img class="icon-img" src="/icons/map-icon-white.svg">
+            </v-btn>
+            <v-menu transition="slide-y-transition">
+                <template v-slot:activator="{ props }">
+                    <v-btn icon v-bind="props">
+                        <img class="icon-img" src="/icons/shopping-bag-icon-white.svg">
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="(item, i) in bag" :key="i">
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            <v-menu transition="slide-y-transition">
+                <template v-slot:activator="{ props }">
+                    <v-btn icon v-bind="props">
+                        <img class="icon-img" src="/icons/user-icon-white.svg">
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="(item, i) in sesion" :key="i" :to="item.path">
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </div>
     </v-toolbar>
 </template>
 
@@ -25,20 +51,35 @@
         name: 'headerView',
         data(){
             return{
-                menu: [
+                sesion:[
                     {
-                        path: '/bag',
-                        src: '/icons/shopping-bag-icon-white.svg'
+                        path: 'https://www.instagram.com/',
+                        title: 'Iniciar sesión'
                     },
                     {
-                        path: '/map',
-                        src: '/icons/map-icon-white.svg'
-                    },
+                        path: 'https://www.facebook.com/',
+                        title: 'Regristarse'
+                    }
+                ],
+                bag:[
                     {
-                        path: '/user',
-                        src: '/icons/user-icon-white.svg'
+                        title: 'Mostrar carrito'
                     }
                 ]
+                // menu: [
+                //     {
+                //         path: '/bag',
+                //         src: '/icons/shopping-bag-icon-white.svg'
+                //     },
+                //     {
+                //         path: '/map',
+                //         src: '/icons/map-icon-white.svg'
+                //     },
+                //     {
+                //         path: '/user',
+                //         src: '/icons/user-icon-white.svg'
+                //     }
+                // ]
             }
         }
     }
